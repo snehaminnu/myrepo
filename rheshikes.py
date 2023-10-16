a64d9030f065d75b1fa363d9e990f523a2a55cd1
oneteamanagha <oneteamanagha.com>
2023-10-16 10:11:59 +0530

    commit msg

---
+++ rheshikes.py
@@ -0,0 +1,4 @@
+
+
+name=input("Enter your name")
+print("Your name is", name)
---
+++ w.py
@@ -0,0 +1,9 @@
+no1=int(input("enter number"))
+fact=1
+for i in range(1,no1+1):
+    fact*=i
+print(fact)
+    
+
+        
+    
